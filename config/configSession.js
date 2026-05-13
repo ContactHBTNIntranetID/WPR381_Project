9b8d29a1f63d5f65171a3c2b50cf11469d01afa9
Eduard <eduardherman11@gmail.com>
2026-05-13 16:57:59 +0200

    Fix update
    Stil need to sort out controllers

--- config/configSession.js
+++ config/configSession.js
@@ -1,16 +1,16 @@
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');   // <-- note the destructuring
 
 module.exports = {
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 // 24 hours
-    },
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI,
-        ttl: 24 * 60 * 60 // 1 day
-    })
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 // 24 hours
+  },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    ttl: 24 * 60 * 60
+  })
 };
